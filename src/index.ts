a35db69c14ef50527b8d77e76dde113a8276c1a5
choice.qin <choice.qin@nirvana-info.com>
2018-05-10 11:56:24 +0800

    change theme

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import 'golden-layout/src/css/goldenlayout-base.css'
-import 'golden-layout/src/css/goldenlayout-light-theme.css'
+import 'golden-layout/src/css/goldenlayout-dark-theme.css'
 import layoutGolden from './golden.vue'
 import glComponent from './gl-component.vue'
 import {glRow, glCol, glStack} from './gl-group'
